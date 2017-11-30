21f5b4dbc6422db742a5ebff4f34764ed43a1fcf
David Basil <info@basildavid.com>
2017-11-30 09:52:54 +0400

    add mongoose and articles model

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
 var express = require('express')
 var path = require('path')
+var mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/movieapp')
+var db = mongoose.connection
+
+// check connection
+db.once('open', function(){
+	console.log('Connected to the database')
+})
+
+// check for db errors
+db.on('error', function(err){
+	console.log(err)
+})
+
 var app = express()
+
+// article model
+var Article = require('./models/article')
 
 // views
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+// get homepage
 app.get('/', function(req, res){
-	var articles = [
-		{
-			"id": 1,
-			"title": "Article One",
-			"author": "David Basil",
-			"body": "This is article one"
+	Article.find({}, function(err, articles){
+		if(err){
+			console.log(err)
+		} else {
+			res.render('index', {
+				"title": "title",
+				"articles": articles
+			})
 		}
-	]
-	res.render('index', {
-		"title": "title"
 	})
 })
 
-// add article page
+// get article page
 app.get('/articles/add', function(req, res){
 	res.render('add-article', {
 		"title": "Add Article"
 	})	
+})
+
+// post article
+app.post('/articles/add', function(req, res){
 })
 
 app.listen(3000, function(){
